0c4b99a6f03fc0133bc7face2ee626cf3faba42a
RAPHAEL YURI DE FRANÇA FERREIRA <RAPHAEL.FERREIR@csi.aneas.msft>
2018-08-24 21:58:06 -0300

    Alterei esse codigo

--- pedidos.php
+++ pedidos.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -10,25 +10,53 @@
     <link rel="stylesheet" href="estilo.css"> 
     <title>Página de pedidos</title>
   </head>
-  
-    <h2>Página de pedidos</h2>
+  <body>
+    <div class="topo">
+    <!-- Código de JavaScript do Bootstrap -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <!-- Login -->
+    <?php
+       include 'loginhome.php';
+    ?>
+    <!-- Fim do Login -->
+    <!--- Logo -->
+    <?php
+       include 'logo.php';
+    ?>
+      </div>
+    <!-- Fim da Logo -->
+
+    
+    
+    <!-- Caixa de pedidos -->
+    <div class="container" id="container-pedidos">
+        
+    
+    <h2 id="pag-pedidos">Página de pedidos</h2>
     <hr>
     <h3>Item</h3>
+
+    <form action="mailto:testejuana@gmail.com" method="post">
+        <div id="itens">
+            <input type="radio" id="item" value="item-caneca" onchange="mostra()"> Caneca
+            <input type="radio" id="item" value="item-chinelo"> Chinelo
+            <input type="radio" id="item" value="item-camisa"> Camisa
+            <input type="radio" id="item" value="item-bolsa"> Bolsa
+            <input type="radio" id="item" value="item-outros"> Outros
+        </div>
+        <script>
+        function mostra(){
+            var tipo = document.getElementById("item").value;
+            
+            if(tipo==="item-caneca"){
+             document.getElementById("Caneca").style.display = "block";   
+            }
+        }
+        </script>
    
-    
-<!-- Caixas de seleção de itens -->
-<form action="mailto:testejuana@gmail.com" method="post">
-    <div class="form-group">
-      <label for="caixa-itens">Selecione o Item de sua preferência</label>
-      <select class="form-control" id="caixa-itens" style="width: 110px">
-          <option value="item-caneca" >Caneca</option>
-          <option value="item-chinelo">Chinelo</option>
-          <option value="item-camisa">Camisa</option>
-          <option value="item-bolsa">Bolsa</option>
-          <option value="item-outros">Outros</option>
-      </select>
-    </div>
-    <!-- Caixas de Tipos de Itens -->
+        <!-- Caixas de Tipos de Itens -->
         
         
         <!-- Canecas -->
@@ -37,47 +65,47 @@
             <h3>Tipo</h3>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-comum">
-              <label class="form-check-label" for="caneca-comum"> Caneca Comum </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca Comum </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-magica">
-              <label class="form-check-label" for="caneca-magica"> Caneca Mágica </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca Mágica </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-colorida">
-              <label class="form-check-label" for="caneca-colorida"> Caneca com alça colorida </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca com alça colorida </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-tarjabranca">
-              <label class="form-check-label" for="caneca-tarjabranca"> Caneca com tarja branca </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca com tarja branca </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-chop">
-              <label class="form-check-label" for="caneca-chop"> Caneca de chop </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca de chop </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-cromada">
-              <label class="form-check-label" for="caneca-cromada"> Caneca cromada </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca cromada </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-fundo">
-              <label class="form-check-label" for="caneca-fundo"> Caneca de fundo colorido </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca de fundo colorido </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="caneca-metalizada">
-              <label class="form-check-label" for="caneca-metalizada"> Caneca metalizada  </label>
+              <label class="form-check-label" for="defaultCheck1"> Caneca metalizada  </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="squeeze">
-              <label class="form-check-label" for="squeeze"> Squeeze </label>
+              <label class="form-check-label" for="defaultCheck1"> Squeeze </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="copo-japones">
-              <label class="form-check-label" for="copo-japones"> Copo Japonês </label>
+              <label class="form-check-label" for="defaultCheck1"> Copo Japonês </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="checkbox" value="" id="copo-cafe">
-              <label class="form-check-label" for="copo-cafe"> Copo de Café </label>
+              <label class="form-check-label" for="defaultCheck1"> Copo de Café </label>
             </div>
         </div>
         
@@ -246,8 +274,8 @@
               <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
               <label class="form-check-label" for="defaultCheck1"> Quebra-cabeças retangular ( 80 peças )  </label>
             </div>
-         </div>
-
+           
+    </div>
     
     
     <table width="32%" border="0" align="text-left">
@@ -260,15 +288,28 @@
     <td><textarea name="mensagem" cols="50" rows="07" id="mensagem"></textarea></td><br>
     </tr>
     <tr>
+    <td></td>
     <td><input type="submit" class="btn btn-success" name="Submit" value="Fazer pedido"></td>
     </tr>
     </table>
     
-</form>
     
-
+    
+    </form>
+    
+      </div>
+      <!-- rodapé -->
+    <footer class="cor-botao p-4" >
+    <div class="botao btn btn bg-white p-0 mr-2">
+        <img src="img/social/facebook.png" alt="" >
+    </div>
+    <div class="btn btn bg-white p-0">
+        <img src="img/social/telegram.png" alt="">
+    </div>
+    </footer>
+    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  
+  </body>
 </html>
